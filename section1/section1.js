cb1994ae958ca22855e75304973d1c95b9e65145
pagiron <pagiron@scl.edu.gt>
2024-05-30 10:45:40 -0600

    Creaciòn de carpetas

---
+++ section1/section1.js
@@ -0,0 +1,3 @@
+let section1_DOM=document.querySelector(".section1");
+
+section1_DOM.innerHTML='Hola soy section 1';
---
+++ section3/section3.js
@@ -0,0 +1,3 @@
+let section3_DOM=document.querySelector(".section3");
+
+section3_DOM.innerHTML='Hola soy section 3';
