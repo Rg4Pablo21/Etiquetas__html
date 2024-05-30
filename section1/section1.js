52c53f3b55cd6ec76c2a253cc69878ba4254bdb3
davillatoro <davillatoro@scl.edu.gt>
2024-05-30 16:37:37 -0600

    version_section1_diego

--- section1/section1.js
+++ section1/section1.js
@@ -1,3 +1,43 @@
 let section1_DOM=document.querySelector(".section1");
 
-section1_DOM.innerHTML='Hola soy section 1';
+section1_DOM.innerHTML=`<div class="seccion1">
+<h1>Bloques</h1>
+<table>
+    <thead>
+        <tr>
+            <th>Etiqueta</th>
+            <th>Descripción</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>&lt;div&gt;</td>
+            <td>Contenedor genérico</td>
+        </tr>
+        <tr>
+            <td>&lt;header&gt;</td>
+            <td>Encabezado de una sección o página</td>
+        </tr>
+        <tr>
+            <td>&lt;footer&gt;</td>
+            <td>Pie de página de una sección o página</td>
+        </tr>
+        <tr>
+            <td>&lt;section&gt;</td>
+            <td>Sección genérica de contenido</td>
+        </tr>
+        <tr>
+            <td>&lt;article&gt;</td>
+            <td>Artículo autónomo</td>
+        </tr>
+        <tr>
+            <td>&lt;aside&gt;</td>
+            <td>Contenido lateral o auxiliar</td>
+        </tr>
+        <tr>
+            <td>&lt;nav&gt;</td>
+            <td>Navegación</td>
+        </tr>
+    </tbody>
+</table>
+</div>`;
